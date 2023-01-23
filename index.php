bdbeddb76b1d56e5ad1a1915b3d6981ca24743da
pennywiseplourde <71292433+pennywiseplourde@users.noreply.github.com>
2023-01-22 21:55:38 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mayhew-Plourde Server</title>
+</head>
+<body>
+<h1>Hello, and welcome to the Mayhew-Plourde Server Main Directory.</h1>
+<h2>Choose a category from the list below.</h2>
+<a href="/Parker/index.php">Parker Personal</a>
+<br>
+<a href="/Passwords/index.php">Passwords</a>
+<div>
+<br>
+<br>
+<p>Created by Parker. <br><br> If you have any issues:<br>Tell me in person<br>Text (207)951-0186<br> Email theparkerman8@gmail.com</p>
+</div>
+</body>
+</html>
